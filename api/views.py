75fcf1c0ee1ac3ada97732ee767fc83a5b51a2f8
Noah <noahtochukwu10@gmail.com>
2024-07-31 18:50:45 +0100

    added JSONRenderer

--- api/views.py
+++ api/views.py
@@ -2,8 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import FormSubmissionSerializer
+from rest_framework.renderers import JSONRenderer
 
 class FormSubmissionView(APIView):
+    renderer_classes = [JSONRenderer]  # This ensures JSON is always returned
+
     def post(self, request, format=None):
         serializer = FormSubmissionSerializer(data=request.data)
         if serializer.is_valid():
