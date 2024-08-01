037e844da40a3a95b1b5d4d485ce7a8708bb1e76
Noah <noahtochukwu10@gmail.com>
2024-08-01 19:35:38 +0100

    Added Authentication

--- formid/urls.py
+++ formid/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('formidify/', include('api.urls')),
+    path('auth/', include('user.urls')),
 
 ]
