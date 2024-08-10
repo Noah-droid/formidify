4e499426f126a1c7138bc0c97a5a000aa654bf91
Noah <noahtochukwu10@gmail.com>
2024-08-11 00:49:20 +0100

    Refixed the Form Submission

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Form
+from .models import Form, FormSubmission
 from rest_framework.renderers import JSONRenderer
 from rest_framework.permissions import IsAuthenticated
 from django.urls import reverse
@@ -9,48 +9,38 @@ from django.shortcuts import get_object_or_404
 
 class CreateFormView(APIView):
     permission_classes = [IsAuthenticated]
+    
     def post(self, request):
-        new_form = Form.objects.create()
-        form_url = request.build_absolute_uri(reverse('submit-form', kwargs={'unique_id': new_form.unique_id}))
-        return Response({'form_url': form_url}, status=status.HTTP_201_CREATED)
+        form_name = request.data.get('name', 'Untitled Form')
+        new_form = Form.objects.create(name=form_name, created_by=request.user)
+        form_url = request.build_absolute_uri(reverse('submit-form', kwargs={'id': new_form.id}))
+        return Response({'form_id': new_form.id, 'form_url': form_url}, status=status.HTTP_201_CREATED)
+
     
 
 class FormSubmissionView(APIView):
     renderer_classes = [JSONRenderer]
 
-    def post(self, request, unique_id):
-        form = get_object_or_404(Form, unique_id=unique_id)
-        
-        new_submission = request.data
-        
-        if form.data:
-            if isinstance(form.data, list):
-                form.data.append(new_submission)
-            else:
-                form.data = [form.data, new_submission]
-        else:
-            form.data = [new_submission]
-        
-        form.save()
+    def post(self, request, id):
+        form = get_object_or_404(Form, id=id)
+        new_submission = FormSubmission.objects.create(form=form, data=request.data)
         return Response({"message": "Form submitted successfully"}, status=status.HTTP_201_CREATED)
 
+
 class FormListView(APIView):
-    def get(self, request, unique_id):
-        form = get_object_or_404(Form, unique_id=unique_id)
-        
-        # Check if there are any submissions
-        if not form.data:
+    
+    def get(self, request, id):
+        form = get_object_or_404(Form, id=id)
+        submissions = form.submissions.all()
+
+        if not submissions:
             return Response({"message": "No submissions yet for this form."}, status=status.HTTP_200_OK)
-        
-        # If the data is a list of submissions
-        if isinstance(form.data, list):
-            submissions = form.data
-        else:
-            # If it's a single submission, wrap it in a list
-            submissions = [form.data]
+
+        submissions_data = [{"id": submission.id, "data": submission.data, "submitted_at": submission.submitted_at} for submission in submissions]
         
         return Response({
-            "form_id": str(form.unique_id),
-            "submissions_count": len(submissions),
-            "submissions": submissions
+            "form_id": str(form.id),
+            "form_name": form.name,
+            "submissions_count": submissions.count(),
+            "submissions": submissions_data
         }, status=status.HTTP_200_OK)
