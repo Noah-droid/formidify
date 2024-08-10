590da0b64881ea583f39329eba9838baa82927af
Noah <noahtochukwu10@gmail.com>
2024-08-11 00:50:03 +0100

    Refixed the Form Submission

--- api/models.py
+++ api/models.py
@@ -1,10 +1,22 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class Form(models.Model):
-    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    data = models.JSONField(default=list)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Form {self.unique_id} submitted at {self.created_at}"
+        return self.name
+
+
+class FormSubmission(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    form = models.ForeignKey(Form, on_delete=models.CASCADE, related_name='submissions')
+    data = models.JSONField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Submission {self.id} to form {self.form.name} at {self.submitted_at}"
