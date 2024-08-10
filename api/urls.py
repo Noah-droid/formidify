fee7d58ef6a5d7a28f819781316d11f423273d3e
Noah <noahtochukwu10@gmail.com>
2024-08-11 00:49:35 +0100

    Refixed the Form Submission

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from .views import CreateFormView, FormSubmissionView, FormListView
 
 urlpatterns = [
     path('create-form/', CreateFormView.as_view(), name='create-form'),
-    path('submit/<uuid:unique_id>/', FormSubmissionView.as_view(), name='submit-form'),
-    path('submissions/<uuid:unique_id>/', FormListView.as_view(), name='form-submissions'),
+    path('submit-form/<uuid:id>/', FormSubmissionView.as_view(), name='submit-form'),
+    path('form-submissions/<uuid:id>/', FormListView.as_view(), name='form-submissions'),
 ]
